6bfdf374fc39d74815919dc7401e5fe7c1734d3a
Tim Ogden <togden1999@gmail.com>
2018-12-23 11:55:03 -0500

    Doing the basics over again

---
+++ tuplesStringsLoops.py
@@ -0,0 +1,11 @@
+#Introduction to variables
+#All this stuff is way too easy rn so let's just speed through it
+programming_languages = "Java", "Python", "C++", "C#", "Ruby"
+
+print(type(programming_languages))
+
+for language in programming_languages:
+    print(language)
+
+for i in range(0, len(programming_languages)):
+    print(programming_languages[i])
